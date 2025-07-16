ac4a6de488f1da3af72a9725176a9323eef289a8
YANNICK kabunga <yankbg2@gmail.com>
2025-07-16 16:53:32 +0300

    feat: Add SplashActivity with delayed transition to MainActivity                                                                      - Implemented `SplashActivity` to display a splash screen.                                                                      - Added a 3-second delay using `Handler` before transitioning to `MainActivity`.                                                                      - Set up the layout for the splash screen in `activity_splash.xml`

---
+++ app/src/main/java/com/example/chatboxapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.chatboxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
