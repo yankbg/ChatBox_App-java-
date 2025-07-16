8008021fd3129943652444ad46a8c8576ee93134
YANNICK kabunga <yankbg2@gmail.com>
2025-07-16 17:37:14 +0300

    The `LoginOtp` activity sets up its layout from `activity_login_otp.xml`, featuring UI components such as an `EditText` for OTP input, a `Button` for navigation, and a `ProgressBar` for loading indication. It provides a user interface for entering the OTP sent to the user's phone number, includes a prompt message, and prepares functionality for OTP submission and navigation to the next activity.

---
+++ app/src/main/java/com/example/chatboxapp/LoginUserName.java
@@ -0,0 +1,14 @@
+package com.example.chatboxapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginUserName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_user_name);
+    }
+}
