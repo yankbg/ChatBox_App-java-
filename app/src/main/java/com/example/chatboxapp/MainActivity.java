18ce248bca4ab8d6c97405c18a382379f239440a
YANNICK kabunga <yankbg2@gmail.com>
2025-07-25 14:03:09 +0300

    Add MainActivity with BottomNavigationView to switch between chat and profile fragments; implement search button to launch SearchUserActivity. Create charFragment and ProfileFragment classes with corresponding XML layouts (fragment_char and fragment_profile) showing placeholder TextViews. Define activity_main.xml layout with a toolbar containing app title and search icon, a FrameLayout for fragment container, a MaterialDivider, and configured BottomNavigationView using new bottom_navigation_menu.xml providing menu items for chats and profile with icons. This commit sets up the foundational UI and navigation structure for the app’s main user interface.

--- app/src/main/java/com/example/chatboxapp/MainActivity.java
+++ app/src/main/java/com/example/chatboxapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.chatboxapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ImageButton searchBtn;
+    charFragment CharFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CharFragment = new charFragment();
+        profileFragment = new ProfileFragment();
+        searchBtn = findViewById(R.id.search_icon);
+        bottomNavigationView = findViewById(R.id.bottomnavigation);
+
+        searchBtn.setOnClickListener(v -> startActivity(new Intent(this,SearchUserActivity.class)));
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.menu_chat){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,CharFragment).commit();
+                }
+                if(item.getItemId() == R.id.menu_profile){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,profileFragment).commit();
+                }
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
     }
 }
